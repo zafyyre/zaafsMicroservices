cac2f6010f7531b2b310dff2d9ebcef574915b39
zafyyre <zaaf17@hotmail.com>
2023-11-28 19:11:10 -0800

    Removed port number in dashboard

--- dashboard-ui/src/components/EndpointAudit.js
+++ dashboard-ui/src/components/EndpointAudit.js
@@ -9,7 +9,7 @@ export default function EndpointAudit(props) {
     const [index, setIndex] = useState(null); // Add state for the index
 
     const getAudit = () => {
-        fetch(`http://acit3855-lab6a.westus.cloudapp.azure.com:8110/${props.endpoint}?index=${rand_val}`)
+        fetch(`http://acit3855-lab6a.westus.cloudapp.azure.com/${props.endpoint}?index=${rand_val}`)
             .then(res => res.json())
             .then((result)=>{
 				console.log("Received Audit Results for " + props.endpoint)
