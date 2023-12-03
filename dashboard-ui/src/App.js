cc71127f9b712186955214c7aaa02e8fded35e02
zafyyre <zaaf17@hotmail.com>
2023-12-03 15:20:06 -0800

    Added openapi specification

--- dashboard-ui/src/App.js
+++ dashboard-ui/src/App.js
@@ -1,30 +1,32 @@
-import React from 'react';
 import logo from './logo.png';
 import './App.css';
 
-import EndpointAudit from './components/EndpointAudit';
-import AppStats from './components/AppStats';
-import ServiceStatus from './components/ServiceStatus'; // Import the new component
+import EndpointAudit from './components/EndpointAudit'
+import AppStats from './components/AppStats'
+import ServiceStatus from './components/ServiceStatus'
 
 function App() {
-    const endpoints = ["teams", "players"];
+
+    const endpoints = ["teams", "players"]
 
     const rendered_endpoints = endpoints.map((endpoint) => {
-        return <EndpointAudit key={endpoint} endpoint={endpoint}/>;
-    });
+        return <EndpointAudit key={endpoint} endpoint={endpoint}/>
+    })
 
     return (
         <div className="App">
             <img src={logo} className="App-logo" alt="logo" height="150px" width="400px"/>
             <div>
                 <AppStats/>
+                <ServiceStatus/>
                 <h1>Audit Endpoints</h1>
                 {rendered_endpoints}
-                <h1>Service Health Status</h1> {/* Add a title for the new section */}
-                <ServiceStatus/> {/* Include the new ServiceStatus component */}
             </div>
         </div>
     );
+
 }
+
+
 
 export default App;
--- dashboard-ui/src/components/ServiceStatus.js
+++ dashboard-ui/src/components/ServiceStatus.js
@@ -1,45 +1,56 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'
 import '../App.css';
 
-export default function ServiceStatus() {
+export default function AppStats() {
     const [isLoaded, setIsLoaded] = useState(false);
-    const [services, setServices] = useState(null);
-    const [error, setError] = useState(null);
+    const [stats, setStats] = useState({});
+    const [error, setError] = useState(null)
 
-    const getHealthStatus = () => {
+	const getStats = () => {
+	
         fetch(`http://acit3855-lab6a.westus.cloudapp.azure.com/health`)
             .then(res => res.json())
-            .then((result) => {
-                console.log("Received Health Status")
-                setServices(result);
+            .then((result)=>{
+				console.log("Received Stats")
+                setStats(result);
                 setIsLoaded(true);
-            }, (error) => {
-                setError(error);
+            },(error) =>{
+                setError(error)
                 setIsLoaded(true);
             })
     }
-
     useEffect(() => {
-        const interval = setInterval(() => getHealthStatus(), 20000); 
-        return () => clearInterval(interval);
-    }, []);
+		const interval = setInterval(() => getStats(), 2000); // Update every 2 seconds
+		return() => clearInterval(interval);
+    }, [getStats]);
 
-    if (error) {
-        return (<div className={"error"}>Error: {error.message}</div>);
-    } else if (!isLoaded) {
-        return (<div>Loading...</div>);
-    } else {
-        return (
+    if (error){
+        return (<div className={"error"}>Error found when fetching from API</div>)
+    } else if (isLoaded === false){
+        return(<div>Loading...</div>)
+    } else if (isLoaded === true){
+        return(
             <div>
-                <h3>Service Health Status</h3>
-                <ul>
-                    {Object.entries(services).map(([service, status]) => (
-                        <li key={service}>
-                            {service}: {status.status} (Last Checked: {status.last_checked})
-                        </li>
-                    ))}
-                </ul>
+                <h1>Running Services</h1>
+                <table className={"StatsTable"}>
+					<tbody>
+						<tr>
+                        <td colspan="2">Audit: {stats['audit_log']}</td>
+						</tr>
+						<tr>
+							<td colspan="2">Processing: {stats['processing']}</td>
+						</tr>
+						<tr>
+							<td colspan="2">Receiver: {stats['receiver']}</td>
+						</tr>
+						<tr>
+							<td colspan="2">Storage: {stats['storage']}</td>
+						</tr>
+					</tbody>
+                </table>
+                <h3>Last Updated: {stats['last_updated']}</h3>
+
             </div>
-        );
+        )
     }
 }
